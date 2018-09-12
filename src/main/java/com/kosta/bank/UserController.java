d65478457b018c88be6ee0281f7e8cb467b1ea2b
existmaster <existmaste@naver.com>
2018-09-12 17:28:03 +0900

    Initial commit

---
+++ src/main/java/com/kosta/bank/UserController.java
@@ -0,0 +1,31 @@
+package com.kosta.bank;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    //@GetMapping(path = "users")
+    //@RequestMapping(path = "/hello", method = RequestMethod.GET)
+    public List<User> hello(){
+        List<User> users = userRepository.findAll();
+        return users;
+    }
+
+    //@PostMapping(path = "users")
+    //@RequestMapping(path = "/hello", method = RequestMethod.POST)
+    public User hello2(@RequestBody User user){
+        User result = userRepository.save(user);
+        return result;
+    }
+}
---
+++ src/main/java/com/kosta/bank/UserRepository.java
@@ -0,0 +1,8 @@
+package com.kosta.bank;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path = "users", collectionResourceRel = "users")
+public interface UserRepository extends JpaRepository<User,Long> {
+}
